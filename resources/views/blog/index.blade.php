29edbab8f4f493a9195c96b6e0d92c8467b742aa
Sergey Dmitriev <dmitrieffserzh@yandex.ru>
2020-07-16 17:20:00 +0300

    update

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -6,6 +6,20 @@
         <div class="section-header__subtitle">Таким образом сложившаяся структура организации играет важную роль</div>
     </div>
     @for($i = 0; $i <= 10; $i++)
+
+        <article class="article" data-img="/uploads/images/0.jpg">
+            <header class="article__header">
+                <h2>Таким образом сложившаяся структура организации играет важную роль в формировании направлений прогрессивного развития</h2>
+            </header>
+
+        </article>
+
+
+
+
+
+<!--
+
         <article class="article">
             <header class="article__header">
                 <h2>Таким образом сложившаяся структура организации играет важную роль в формировании направлений прогрессивного развития</h2>
@@ -20,7 +34,59 @@
                 <a href="#" class="article__more-link">Читать далее...</a>
             </footer>
         </article>
+-->
+
+
+
     @endfor
+
+
+    <script>
+
+        $(window).scroll(function() {
+
+            if ($(window).scrollTop() > 5) {
+                $('.header').addClass('scroll');
+            } else {
+                $('.header').removeClass('scroll');
+            }
+        });
+
+
+
+        $(document).ready(function () {
+            var articles = $(".article");
+
+
+            $.each(articles, function(index, value){
+                console.log("INDEX: " + index + " VALUE: " + value);
+
+                randColor(value);
+
+            });
+
+            function randColor(elem) {
+                var element = elem,
+                    //code_color = document.querySelector('.code_color'),
+                    r = Math.floor(Math.random() * (256)),
+                    g = Math.floor(Math.random() * (256)),
+                    b = Math.floor(Math.random() * (256)),
+                    color = '#' + r.toString(16) + g.toString(16) + b.toString(16);
+
+
+                var img = $(element).data('img');
+
+                $(element).css({
+                    'background':'linear-gradient(135deg, rgba(0, 0, 0, 0.5) 0%, '+ color +' 100%), url('+ img +') no-repeat'
+                }) ;
+                //code_color.innerText = color;
+            }
+
+        });
+
+    </script>
+
+
 @endsection
 
 @section('sidebar')
--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -9,13 +9,20 @@
         <meta name="keywords" content="Ключевые слова, и, фразы, через, запятую">
         <meta name="description" content="Описание контента страницы, 1-2 предложения.">
 
-        <script src="{{ asset('js/app.js') }}" defer></script>
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="{{ asset('js/app.js') }}" defer></script>
+
     </head>
     <body>
         <header class="header">
             <div class="container">
-                HEADER
+                <div class="logo">
+                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="40" height="40" viewBox="0 0 40.1 40">
+                        <polygon fill="#000" points="0,0 0,40 10.1,30 20.1,40 30.1,30 "/>
+                        <polygon fill="#FF0849" points="40.1,0 40.1,40 20.1,20 "/>
+                    </svg>
+                </div>
             </div>
         </header>
         <main class="main container">
